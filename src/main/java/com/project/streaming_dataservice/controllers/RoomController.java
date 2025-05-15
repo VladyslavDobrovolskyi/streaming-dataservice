076607dda53052426b996be61ae6db7a16cf6e11
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-15 23:58:44 +0300

    Add HTTP status handling and request mapping for room operations in RoomController

--- src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
+++ src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
@@ -1,9 +1,14 @@
 package com.project.streaming_dataservice.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.project.streaming_dataservice.service.RoomService;
+import com.project.streaming_dataservice.models.Room;
 
 @RestController
 @RequestMapping("/api/room")

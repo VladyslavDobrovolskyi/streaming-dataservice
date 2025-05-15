bcdeec85c73674ca7fda9af838ff507ffe23dcc2
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-15 23:16:45 +0300

    Add RequestMapping import to RoomController

--- src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
+++ src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
@@ -1,6 +1,7 @@
 package com.project.streaming_dataservice.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.project.streaming_dataservice.service.RoomService;
 

945c8ab9f17c8c31d7565ec9be1183e30440780d
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-16 00:04:59 +0300

    Remove unused imports in RoomController for cleaner code

--- src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
+++ src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
@@ -7,11 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
 import com.project.streaming_dataservice.model.Room;
-
 import com.project.streaming_dataservice.service.RoomService;
-
 import java.security.Principal;
 
 @RestController

9be97b2fab37a051f02a0959a05ec447a84acb72
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-15 22:46:18 +0300

    Update SecurityConfig to remove deprecated hello endpoint from CSRF exceptions

--- src/main/java/com/project/streaming_dataservice/config/SecurityConfig.java
+++ src/main/java/com/project/streaming_dataservice/config/SecurityConfig.java
@@ -19,9 +19,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/register", "/api/users/hello", "/api/users/login", "/api/users/update", "/api/users/delete", "/api/room/hello"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/register", "/api/users/login", "/api/users/update", "/api/users/delete", "/api/room/hello"))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/register", "/api/users/hello", "/api/users/login", "/api/users/update", "/api/users/delete").permitAll()
+                        .requestMatchers("/api/users/register", "/api/users/login", "/api/users/update", "/api/users/delete", "/api/room/hello").permitAll()
 //                        .requestMatchers("/api/users/update", "/api/users/delete").authenticated()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))

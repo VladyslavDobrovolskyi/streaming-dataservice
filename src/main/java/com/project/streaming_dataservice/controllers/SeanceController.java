42cde9a417325df1aca718df9be3c20cc838d0ce
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-16 13:13:35 +0300

    Update SeanceController to use Jakarta validation package

--- src/main/java/com/project/streaming_dataservice/controllers/SeanceController.java
+++ src/main/java/com/project/streaming_dataservice/controllers/SeanceController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
+import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api/seances")
 public class SeanceController {

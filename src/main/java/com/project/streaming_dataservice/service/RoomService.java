791f2ff6fe95966e7ab6a7cfdc2eee5943ea4f85
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-16 15:44:50 +0300

    Update deleteInactiveRooms scheduling to run every minute and adjust cutoff time to 2 minutes

--- src/main/java/com/project/streaming_dataservice/service/RoomService.java
+++ src/main/java/com/project/streaming_dataservice/service/RoomService.java
@@ -83,9 +83,9 @@ public class RoomService {
     }
 
       @Transactional
-    @Scheduled(fixedRate = 5 * 60 * 1000) // запускать каждые 5 минут
+    @Scheduled(fixedRate = 1 * 60 * 1000) // запускать каждые 5 минут
     public void deleteInactiveRooms() {
-        LocalDateTime cutoff = LocalDateTime.now().minusMinutes(10);
+        LocalDateTime cutoff = LocalDateTime.now().minusMinutes(2);
         List<Room> rooms = roomRepository.findByCreatedAtBefore(cutoff);
         
         for (Room room : rooms) {

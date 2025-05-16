2dead3ddf083dd409022ffc10514ae67ceef68c1
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-16 15:15:18 +0300

    Enable scheduling and add createdAt filtering in RoomRepository

--- src/main/java/com/project/streaming_dataservice/StreamingDataServiceApplication.java
+++ src/main/java/com/project/streaming_dataservice/StreamingDataServiceApplication.java
@@ -3,6 +3,8 @@ package com.project.streaming_dataservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
--- src/main/java/com/project/streaming_dataservice/repos/RoomRepository.java
+++ src/main/java/com/project/streaming_dataservice/repos/RoomRepository.java
@@ -2,14 +2,15 @@ package com.project.streaming_dataservice.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.time.LocalDateTime;
+import java.util.List;
 import com.project.streaming_dataservice.model.Room;
 
 import java.util.UUID;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, UUID> {
-    
+
 List<Room> findByCreatedAtBefore(LocalDateTime dateTime);
 
 }

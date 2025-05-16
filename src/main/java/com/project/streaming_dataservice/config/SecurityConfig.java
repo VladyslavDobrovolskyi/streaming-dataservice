788d0966ca0ef6f86639a2c71a6b3dd928ae8347
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-16 08:49:31 +0300

    Disable CSRF protection for room creation and hello endpoints

--- src/main/java/com/project/streaming_dataservice/config/SecurityConfig.java
+++ src/main/java/com/project/streaming_dataservice/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/register", "/api/users/login", "/api/users/update", "/api/users/delete", "/api/room/hello", "/api/room/create"))
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/register", "/api/users/login", "/api/users/update", "/api/users/delete", "/api/room/hello", "/api/room/create").permitAll()
 //                        .requestMatchers("/api/users/update", "/api/users/delete").authenticated()

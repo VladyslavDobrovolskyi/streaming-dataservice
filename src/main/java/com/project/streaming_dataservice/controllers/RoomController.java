01256f468eaa9edb6af5d0e61ce96e827fbeb7ff
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-17 13:56:44 +0300

    Enhance joinRoom endpoint to validate user ID and check user existence before joining

--- src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
+++ src/main/java/com/project/streaming_dataservice/controllers/RoomController.java
@@ -3,6 +3,7 @@ package com.project.streaming_dataservice.controllers;
 import com.project.streaming_dataservice.model.Room;
 import com.project.streaming_dataservice.requests.JoinRoomRequest;
 import com.project.streaming_dataservice.service.RoomService;
+import com.project.streaming_dataservice.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +16,11 @@ import java.util.UUID;
 public class RoomController {
 
     private final RoomService roomService;
+     private final UserService userService;
 
     public RoomController(RoomService roomService) {
         this.roomService = roomService;
+        this.userService = userService;
     }
 
     @PostMapping("/create")
@@ -26,15 +29,30 @@ public class RoomController {
         return new ResponseEntity<>(createdRoom, HttpStatus.CREATED);
     }
 
-  @PostMapping("/join")
-    public ResponseEntity<String> joinRoom(@RequestBody JoinRoomRequest request) {
-        boolean joined = roomService.joinRoom(request.getRoomUUID(), request.getPassword());
-        if (joined) {
-            return ResponseEntity.ok("Successfully joined the room");
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Failed to join the room: Invalid UUID or password");
-        }
+@PostMapping("/join")
+public ResponseEntity<String> joinRoom(
+        @RequestBody JoinRoomRequest request,
+        @CookieValue(value = "userId", required = false) String userId) {
+
+    // Проверка наличия userId
+    if (userId == null || userId.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing or invalid user ID");
     }
+
+    // Проверка существования пользователя
+    boolean userExists = userService.findUserById(userId);
+    if (!userExists) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
+    }
+
+    // Попытка входа в комнату
+    boolean joined = roomService.joinRoom(request.getRoomUUID(), request.getPassword(), userId);
+    if (joined) {
+        return ResponseEntity.ok("Successfully joined the room");
+    } else {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Failed to join the room: Invalid UUID or password");
+    }
+}
 
     @GetMapping("/hello")
     public String helloRoom() {

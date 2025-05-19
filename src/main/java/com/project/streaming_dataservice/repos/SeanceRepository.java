a33abfc2816687263530b66a730ceadb582d938f
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-19 12:07:26 +0300

    Add method to find Seances by last renewal time

--- src/main/java/com/project/streaming_dataservice/repos/SeanceRepository.java
+++ src/main/java/com/project/streaming_dataservice/repos/SeanceRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 public interface SeanceRepository extends JpaRepository<Seance, String> {
     List<Seance> findByOwnerId(String ownerId);
     boolean existsByRoom(Room room);
+    List<Seance> findByLastRenewalBefore(LocalDateTime time);
 
 }

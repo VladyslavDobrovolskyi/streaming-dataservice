b846e7dc37f6345380cd50b4b42d3258d896fce4
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-19 12:03:57 +0300

    Add scheduled import to SeanceService for future task scheduling

--- src/main/java/com/project/streaming_dataservice/service/SeanceService.java
+++ src/main/java/com/project/streaming_dataservice/service/SeanceService.java
@@ -12,7 +12,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.scheduling.annotation.Scheduled;
 import java.util.List;
 import java.util.UUID;
 
